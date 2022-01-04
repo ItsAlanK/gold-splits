a96ccb906e97dd42aa9416bccc6f4a4e20933d88
Alan Kinsella <alankinsella1993@gmail.com>
2022-01-04 21:24:21 +0000

    Remove stray / from ALLOWED_HOSTS in settings.py

--- goldsplits/settings.py
+++ goldsplits/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gold-splits.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['gold-splits.herokuapp.com', 'localhost']
 
 
 # Application definition
