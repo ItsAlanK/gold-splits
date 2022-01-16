c912eb7b8ad5e3ac5ea28f479fe504764de0daca
Alan Kinsella <alankinsella1993@gmail.com>
2022-01-16 21:50:57 +0000

    Add posts page view and template

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('posts', views.AllPosts.as_view(), name='posts')
 ]
--- posts/views.py
+++ posts/views.py
@@ -7,3 +7,9 @@ class PostList(ListView):
     model = Post
     queryset = Post.objects.order_by('-date')
     template_name = 'pages/index.html'
+
+
+class AllPosts(ListView):
+    model = Post
+    queryset = Post.objects.order_by('-date')
+    template_name = 'pages/posts.html'
