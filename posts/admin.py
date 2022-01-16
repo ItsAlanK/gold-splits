e5ad065f8beeb2595ebfdca51b9ced4524f2fb3a
Alan Kinsella <alankinsella1993@gmail.com>
2022-01-16 10:47:02 +0000

    Add prepopulated field for slug based on title

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post, Categories
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Categories)
