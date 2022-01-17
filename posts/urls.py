0a56ca272487b5e744fe269f96bfafbaa97334b6
Alan Kinsella <alankinsella1993@gmail.com>
2022-01-17 18:50:54 +0000

    Create Post page view and template file

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('posts', views.AllPosts.as_view(), name='posts')
+    path('posts', views.AllPosts.as_view(), name='posts'),
+    path('<slug:slug>/', views.Post_detail.as_view(), name='post_page'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,34 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
-class PostList(ListView):
+class PostList(generic.ListView):
+    """Post list which shows on home page"""
     model = Post
     queryset = Post.objects.order_by('-date')
     template_name = 'pages/index.html'
 
 
-class AllPosts(ListView):
+class AllPosts(generic.ListView):
+    """List of all posts for Posts page"""
     model = Post
     queryset = Post.objects.order_by('-date')
     template_name = 'pages/posts.html'
     paginate_by = 10
+
+
+class Post_detail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        comments = post.comments.order_by('date')
+
+        return render(
+            request,
+            "pages/post-page.html",
+            {
+                "post": post,
+                "comments": comments,
+            }
+        )
