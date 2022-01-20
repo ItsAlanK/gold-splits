3cfaa27811087ce61992a93681be686eb1373f50
Alan Kinsella <alankinsella1993@gmail.com>
2022-01-20 17:42:48 +0000

    Fix profiles not loading correctly by changing profile query to check for user instead of pk

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from .models import Profile
+from posts.models import Post
 from django.views import generic, View
 from django.shortcuts import reverse, get_object_or_404, render
+from django.contrib.auth.models import User
 from .forms import ProfileForm
 
 
@@ -15,13 +17,16 @@ class EditProfile(generic.UpdateView):
 
 class ProfileView(View):
 
-    def get(self, request, pk, *args, **kwargs):
-        profile = get_object_or_404(Profile, pk=pk)
+    def get(self, request, name, *args, **kwargs):
+        user_id = User.objects.get(username=name).pk
+        profile = get_object_or_404(Profile, user=user_id)
+        posts = Post.objects.filter(author=profile.user)
 
         return render(
             request,
             "pages/profile/profile-page.html",
             {
                 "profile": profile,
+                "posts": posts
             }
         )
