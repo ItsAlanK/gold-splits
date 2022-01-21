25a83a99cded85a21e02b59eb6784b6d5a26c85f
Alan Kinsella <alankinsella1993@gmail.com>
2022-01-21 09:42:49 +0000

    Move signup form to components to be used in homepage, passed form context to homepage in views

--- posts/views.py
+++ posts/views.py
@@ -17,6 +17,7 @@ class PostList(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["comments"] = Comment.objects.all()
+        context["form"] = SignupForm
         return context
 
 
